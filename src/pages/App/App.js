d2835c7981bb7b9726dd5a34da30bba04bef4d3d
Jesse Zarat <jczarat@gmail.com>
2020-10-12 16:32:26 -0600

    update routing

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
             <div className="App">
                 <header className="App-header">
-                    Auth Template
+                    The Sweat Aesthetic
                     <nav>
                         <NavBar
                             user={this.state.user}
