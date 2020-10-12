f1ef8366eb9896a21fa7a9ec61f85bc864144c1c
Jesse Zarat <jczarat@gmail.com>
2020-10-12 15:55:05 -0600

    add auth

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
-import './App.css';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: userService.getUser()
+        };
+    }
+
+    handleLogout = () => {
+        userService.logout();
+        this.setState({ user: null });
+    }
+
+    handleSignupOrLogin = () => {
+        this.setState({ user: userService.getUser() });
+    }
+
     render() {
         return (
 
             <div className="App">
                 <header className="App-header">
-                    The Sweat Aesthetic
+                    Auth Template
                     <nav>
-                        <NavBar />
+                        <NavBar
+                            user={this.state.user}
+                            handleLogout={this.handleLogout}
+                        />
                     </nav>
                 </header>
 
@@ -19,8 +42,20 @@ class App extends Component {
                     <Route exact path='/' render={() =>
                         <h1>Home Page</h1>
                     } />
-                    <Route exact path='/products' render={() =>
-                        <h1>products Page</h1>
+                    <Route exact path='/show' render={() =>
+                        <h1>Show Page</h1>
+                    } />
+                    <Route exact path='/signup' render={({ history }) =>
+                        <SignupPage
+                            history={history}
+                            handleSignupOrLogin={this.handleSignupOrLogin}
+                        />
+                    } />
+                    <Route exact path='/login' render={({ history }) =>
+                        <LoginPage
+                            handleSignupOrLogin={this.handleSignupOrLogin}
+                            history={history}
+                        />
                     } />
                 </Switch>
             </div>
